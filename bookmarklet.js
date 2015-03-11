f4c70be47fb65790f07d1be22d751f1133b9ea3c
trinetixHTML <eugene.garkavtcev@trinetix.com>
2015-03-11 11:47:15 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,7 @@
+script2=document.createElement('script');
+script2.src='//trinetixhtml.github.io/trash/urls.js';
+document.getElementsByTagName('head')[0].appendChild(script2);
+
 var linksArray = [
 "https://vk.com/feed?z=photo-78543917_354855186%2Falbum-78543917_00%2Frev",
 "http://www.zara.com",

ef6546cd331b5d11066e0b6de7b961bd2cd837ab
trinetixHTML <eugene.garkavtcev@trinetix.com>
2015-03-11 11:50:40 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,19 +2,6 @@ script2=document.createElement('script');
 script2.src='//trinetixhtml.github.io/trash/urls.js';
 document.getElementsByTagName('head')[0].appendChild(script2);
 
-var linksArray = [
-"https://vk.com/feed?z=photo-78543917_354855186%2Falbum-78543917_00%2Frev",
-"http://www.zara.com",
-];
-
-var redirect;
-for (i=0; i<linksArray.length; i++){
-	if (document.location == linksArray[i]){
-		redirect = linksArray[i+1];
-	};
-};
-if (redirect == undefined){
-	redirect = "";
-}
-console.log(redirect);
-//window.location.assign(redirect);
+script3=document.createElement('script');
+script3.src='//trinetixhtml.github.io/trash/funtion.js';
+document.getElementsByTagName('head')[0].appendChild(script3);

98ab433d34aa10de357cb7910010e87fbd70d956
trinetixHTML <eugene.garkavtcev@trinetix.com>
2015-03-11 11:51:18 +0200

    Create function.js

---
+++ function.js
@@ -0,0 +1,11 @@
+var redirect;
+for (i=0; i<linksArray.length; i++){
+	if (document.location == linksArray[i]){
+		redirect = linksArray[i+1];
+	};
+};
+if (redirect == undefined){
+	redirect = "";
+}
+console.log(redirect);
+//window.location.assign(redirect);

26014db9711622bde20e5c32c901287410bd93e1
trinetixHTML <eugene.garkavtcev@trinetix.com>
2015-03-11 11:53:58 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,9 @@
 script2=document.createElement('script');
 script2.src='//trinetixhtml.github.io/trash/urls.js';
 document.getElementsByTagName('head')[0].appendChild(script2);
+console.log(script2);
 
 script3=document.createElement('script');
 script3.src='//trinetixhtml.github.io/trash/function.js';
 document.getElementsByTagName('head')[0].appendChild(script3);
+console.log(script3);
